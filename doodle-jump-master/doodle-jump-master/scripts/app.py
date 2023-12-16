801a68c16670a076218920409b57c92516676ed7
Ganri-Tyyaan <152689498+Ganri-Tyyaan@users.noreply.github.com>
2023-12-16 13:31:36 +0300

    Add files via upload

---
+++ doodle-jump-master/doodle-jump-master/scripts/app.py
@@ -0,0 +1,35 @@
+import pygame
+import os
+from scripts.functions import load_image
+class App:
+    def __init__(self) -> None:
+        pygame.display.set_caption('DOODLE JUMP')
+        pygame.display.set_icon(load_image("assets","icons","icon.ico"))
+
+        self.display_size =(480,720)
+        self.running=True
+        self.maxFPS=567
+
+        self.display=pygame.display.set_mode(self.display_size)
+        self.clock=pygame.time.Clock()
+        self.game=Game()
+        def handle_events(self) -> None:
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    self.running=False
+
+        def update(self) -> None:
+            ...
+        def render(self) -> None:
+            self.display.fill((0,0,0))
+            self.game.render(self.display)
+            pygame.display.update()
+
+        def run(self) -> None:
+            while self.running:
+                self.handle_events()
+                self.update()
+                self.render()
+                self.clock.tick(self.maxFPS)
+
+
