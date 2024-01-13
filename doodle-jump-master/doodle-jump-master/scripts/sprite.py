ec3c054b134332e4dc6e0c4fe81b7a857c97cedb
Ganri-Tyyaan <152689498+Ganri-Tyyaan@users.noreply.github.com>
2024-01-13 13:37:37 +0300

    Delete doodle-jump-master/doodle-jump-master directory

--- doodle-jump-master/doodle-jump-master/scripts/sprite.py
+++
@@ -1,8 +0,0 @@
-class Sprite:
-    def __init__(self,center,image):
-        self.image=image.copy()
-        self.rect=self.image.get_frect()
-        self.rect.center=center
-#blit отображает картинку по координатам/прямо
-    def render(self,surface):
-        surface.blit(self.image,self.rect)
